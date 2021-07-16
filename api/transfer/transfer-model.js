476069b445f6602b49ac71c2852db979a5b205b1
hans0 <kiessler.hans@gmail.com>
2021-07-16 17:25:10 -0400

    still not returning an object, after multiple revisions

--- api/transfer/transfer-model.js
+++ api/transfer/transfer-model.js
@@ -1,17 +1,26 @@
 const db = require("../data/db-config.js");
 
+const transfer = async (objectBarcode, boxBarcode) => {
+  // the await needs to be here, otherwise get an undefined for the box number
+  const res = await db("boxes").select().where("barcode", boxBarcode).first();
+  console.log(res.box_id);
+  const entry = {
+    object_barcode: objectBarcode,
+    box_id: res.box_id,
+  };
+  console.log(entry);
+  const insertRes = await db("barcodes_to_box").insert(entry);
+
+  /*
+  const e = await db("barcodes_to_box")
+    .select("barcode", "box_id")
+    .where("barcode", boxBarcode)
+    .first();
+  console.log("e: ", e);
+  return e;
+  */
+};
+
 module.exports = {
   transfer,
 };
-
-async function transfer(objectBarcode, boxBarcode) {
-  const bResponse = await db("boxes")
-    .select("box_id")
-    .where("barcode", boxBarcode)
-    .first();
-  console.log(bResponse.box_id);
-  return await db("barcodes_to_box").insert({
-    object_barcode: objectBarcode,
-    box_id: bResponse.box_id,
-  });
-}
--- api/transfer/transfer-router.js
+++ api/transfer/transfer-router.js
@@ -4,16 +4,7 @@ const Transfer = require("./transfer-model");
 const Boxes = require("../boxes/boxes-model");
 
 router.post("/", (req, res) => {
-  console.log(req.body);
   const { objectBarcode, boxBarcode } = req.body;
-  console.log(objectBarcode, boxBarcode);
-  // Boxes.findByBarcode(boxBarcode)
-  //   .then((boxId) => {
-  //     res.status(200).json(boxId);
-  //   })
-  //   .catch((err) => {
-  //     res.status(400).json(err);
-  //   });
   Transfer.transfer(objectBarcode, boxBarcode)
     .then((object) => {
       res.status(200).json(object);
